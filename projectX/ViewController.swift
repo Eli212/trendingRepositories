df5671529a9a1db0a70ebbd81100b1dec8ccfeb4
Eli Bunimovich <elibunimovich@elis-macbook-air.local>
2017-11-16 17:34:46 +0200

    Initial Commit

---
+++ projectX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  projectX
+//
+//  Created by Eli Bunimovich on 16/11/2017.
+//  Copyright © 2017 Eli Bunimovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
